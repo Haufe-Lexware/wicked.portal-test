c16eed2d3ac6a8826e3448ea6d72e0fd91663b1e
Martin Danielsson <martin@danielsson.de>
2018-04-27 09:50:10 +0200

    Stub for user registration tests

---
+++ portal-api/test/14_registrationsTest.js
@@ -0,0 +1,17 @@
+var assert = require('chai').assert;
+var request = require('request');
+var utils = require('./testUtils');
+var consts = require('./testConsts');
+
+var baseUrl = consts.BASE_URL;
+
+describe('/registrations', () => {
+
+    // WIP
+    // it('should return a 404 for unknown users', (done) => {
+    //     request.get({
+    //         url: baseUrl + 
+    //     })
+    // });
+    
+});
