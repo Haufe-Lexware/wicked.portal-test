0c9c85f0595b88ad7de3a82cdf8a6d4adedb31b1
Danielsson <martin.danielsson@haufe-lexware.com>
2019-04-01 16:09:38 +0200

    Linting kong-adapter tests

--- portal-kong-adapter/test/testConsts.js
+++ portal-kong-adapter/test/testConsts.js
@@ -5,7 +5,7 @@ const consts = {};
 consts.BASE_URL = checkSlash(process.env.PORTAL_API_URL || 'http://portal-api:3001/');
 consts.KONG_ADAPTER_URL = checkSlash(process.env.KONG_ADAPTER_URL || 'http://portal-kong-adapter:3002/');
 consts.KONG_ADMIN_URL = checkSlash(process.env.KONG_ADMIN_URL || 'http://kong:8001/');
-consts.KONG_GATEWAY_URL = checkSlash(process.env.KONG_GATEWAY_URL || 'http://kong:8000/');
+consts.KONG_GATEWAY_URL = checkSlash(process.env.KONG_GATEWAY_URL || 'http://kong:8000/');
 
 function checkSlash(s) {
     if (!s.endsWith('/'))
